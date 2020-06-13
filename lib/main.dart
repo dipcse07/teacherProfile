e121a62309f200d8235167af5d3acdd60cfb39f4
MD SAZID HASAN DIP <dipcse07@gmail.com>
2020-06-13 23:53:52 +0600

    git commit

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   var collegeDataLenght;
 
+  List<String> keyList;
+
+  String SelectedUniversity;
+
 //  @override
 //  void initState() {
 //    this.getData();
@@ -56,6 +60,11 @@ class _MyHomePageState extends State<MyHomePage> {
       count = collegeDataLenght;
       teachersInfo = data['CollegeName1'];
       print(teachersInfo[0]['avatarImage']);
+
+      keyList =
+          jsonDecode(response.body.toString()).keys.toList() as List<String>;
+
+      print(keyList);
     });
   }
 
@@ -77,67 +86,156 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
                 child: Text('get data'),
               ),
+              DropdownButton<String>(
+                value: null,
+                items: keyList.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+
+                onChanged: (_value) {
+                  //valueChanged(_value);
+                  print(_value);
+                  print(keyList);
+                  keyList = null;
+
+                  Text(
+                    '_value',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  );
+
+                  setState(() {
+                    if (SelectedUniversity != 'Get the Data First') {
+                      SelectedUniversity = _value;
+                    }
+                    print(SelectedUniversity);
+//                  Text(
+//                    selectUniversity,
+//                    textAlign: TextAlign.center,
+//                    style: TextStyle(color: Colors.white),
+//                  );
+                  });
+                },
+
+//                  style: TextStyle(
+//                    color: Colors.white,
+//                  ),
+                hint: Text(
+                  'Select Universities',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
               Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  scrollDirection: Axis.vertical,
-                  children: List.generate(count, (index) {
-                    return Center(
-                      child: Container(
-//                        height: 180,
-//                        width: 180,
-                        //margin: EdgeInsets.all(1.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.only(top: 9.0),
-                              child: CircleAvatar(
-                                radius: 60.0,
-                                backgroundColor: Colors.red,
-                                backgroundImage:
-                                    teachersInfo[index]['avatarImage'] == 'url'
-                                        ? AssetImage('images/dip.jpg')
-                                        : null,
+                child: ListView.builder(
+                    itemCount:
+                        collegeDataLenght == null ? 0 : collegeDataLenght,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                        height: 110,
+                        child: Card(
+                          child: Row(
+                            //mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 8, right: 8),
+                                child: CircleAvatar(
+                                  radius: 45.0,
+                                  backgroundColor: Colors.red,
+                                  backgroundImage:
+                                      data[SelectedUniversity.toString()][index]
+                                                  ['avatarImage'] ==
+                                              'url'
+                                          ? AssetImage('images/url.jpg')
+                                          : null,
+                                ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 2.0),
-                              child: Text(
-                                teachersInfo[index]['teacherName'].toString(),
+                              SizedBox(
+                                width: 10.0,
+                              ),
+                              Text(
+                                data[SelectedUniversity.toString()][index]
+                                        ['teacherName']
+                                    .toString(),
                                 style: TextStyle(
-                                  fontFamily: 'Pacifico',
-                                  fontSize: 20.0,
+                                  //fontFamily: 'Pacifico',
+                                  fontSize: 25.0,
                                   color: Colors.white,
                                   fontWeight: FontWeight.bold,
                                 ),
-                                textAlign: TextAlign.center,
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  }),
-                ),
-              ),
+                      );
+                    }),
+              )
             ],
           ),
         ));
   }
 }
 
-//ListView.builder(
-//itemCount: data == null ? 0 : data['CollegeName1'].length,
-//itemBuilder: (BuildContext context, int index) {
-//return Card(
-//child: Column(
-//children: <Widget>[
-//Text(data['CollegeName1'][index]['teacherName'] == null
-//? 'text'
-//    : data['CollegeName1'][index]['teacherName']),
-//],
-//),
-//);
-//},
+class DipGridView extends StatelessWidget {
+  const DipGridView({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+      scrollDirection: Axis.vertical,
+      children: List.generate(count, (index) {
+        return Center(
+          child: Container(
+//                        height: 180,
+//                        width: 180,
+            //margin: EdgeInsets.all(1.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 9.0),
+                  child: CircleAvatar(
+                    radius: 60.0,
+                    backgroundColor: Colors.black12,
+                    backgroundImage: teachersInfo[index]['avatarImage'] == 'url'
+                        ? AssetImage('images/url.jpg')
+                        : null,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 2.0),
+                  child: Text(
+                    teachersInfo[index]['teacherName'].toString(),
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
